25027eb74f6ee82ff0819827b46032cd63d34ea3
PRAKHARSAHUJI <148934122+PRAKHARSAHUJI@users.noreply.github.com>
2025-02-04 01:11:33 +0530

    Time: 0 ms (100%), Space: 8.1 MB (16.88%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -5,16 +5,41 @@
 //         return fib(n-1) + fib(n-2);
 //     }
 // };
+
+// ----------------------------------------------
+
+
+// class Solution {
+// public:
+//     int fib(int n) {
+//         if(n <= 1) return n;
+//         vector<int>dp(n+1, -1);
+//         dp[0] = 0;
+//         dp[1] = 1;
+//         for(int i = 2; i <= n; i++){
+//             dp[i] = dp[i-1] + dp[i-2];
+//         }
+//         return dp[n];
+//     }
+// };
+
+
+// ----------------------------------------------
+
+
 class Solution {
 public:
-    int fib(int n) {
+    int ans(int n, vector<int>& dp)
+    {
         if(n <= 1) return n;
-        vector<int>dp(n+1, -1);
-        dp[0] = 0;
-        dp[1] = 1;
-        for(int i = 2; i <= n; i++){
-            dp[i] = dp[i-1] + dp[i-2];
+        if(dp[n] == -1){
+            dp[n] = ans(n-1,dp) + ans(n-2,dp);
+            return dp[n];
         }
         return dp[n];
+    }
+    int fib(int n) {
+        vector<int>dp(n+1,-1);
+        return ans(n,dp);
     }
 };
