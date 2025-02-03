638f9ef5b1932c59049aea307bf1a8ed52642220
PRAKHARSAHUJI <148934122+PRAKHARSAHUJI@users.noreply.github.com>
2025-02-04 01:18:23 +0530

    Time: 2 ms (79%), Space: 7.7 MB (86.58%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,3 +1,5 @@
+// NORMAL
+
 // class Solution {
 // public:
 //     int fib(int n) {
@@ -8,6 +10,7 @@
 
 // ----------------------------------------------
 
+// DP BOTTOM UP
 
 // class Solution {
 // public:
@@ -26,20 +29,45 @@
 
 // ----------------------------------------------
 
+// TOP DOWN
+
+// class Solution {
+// public:
+//     int ans(int n, vector<int>& dp)
+//     {
+//         if(n <= 1) return n;
+//         if(dp[n] == -1){
+//             dp[n] = ans(n-1,dp) + ans(n-2,dp);
+//             return dp[n];
+//         }
+//         return dp[n];
+//     }
+//     int fib(int n) {
+//         vector<int>dp(n+1,-1);
+//         return ans(n,dp);
+//     }
+// };
+
+
+// ----------------------------------------------
+
+// SPACE OPTIMIZE
 
 class Solution {
 public:
-    int ans(int n, vector<int>& dp)
-    {
-        if(n <= 1) return n;
-        if(dp[n] == -1){
-            dp[n] = ans(n-1,dp) + ans(n-2,dp);
-            return dp[n];
-        }
-        return dp[n];
-    }
+    
     int fib(int n) {
-        vector<int>dp(n+1,-1);
-        return ans(n,dp);
+        if(n <= 1) return n;
+        int prev2 = 0;
+        int prev1 = 1;
+        int curr = 0;
+        for(int i = 2; i <= n; i++){
+            curr = prev2 + prev1;
+            prev2 = prev1;
+            prev1 = curr;
+
+        }
+        return curr;
+
     }
 };
