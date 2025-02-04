d3691cec22b72b79346d0235dae1c0344aaee369
PRAKHARSAHUJI <148934122+PRAKHARSAHUJI@users.noreply.github.com>
2025-02-04 11:07:43 +0530

    Time: 0 ms (100%), Space: 8.7 MB (17.54%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -67,22 +67,38 @@
 
 
 // ------------------------------------------
-// InCorrect Code
-// TLE
+// Correct Code
+// TOP DOWN
+// class Solution {
+// public:
+//     int count(int i, int n, vector<int>&dp){
+//         if(i == n) return 1;
+//         if(i > n) return 0;
+//         if(dp[i] != -1){
+//             return dp[i];
+//         }
+//         return dp[i] = count(i+1,n,dp) + count(i+2,n,dp); 
+//     }
 
+//     int climbStairs(int n) {
+//         vector<int>dp(n+2,-1);
+//         return count(0,n,dp);
+//     }
+// };
+
+
+// Correct Code
+// BOTTOM UP
 class Solution {
 public:
-    int count(int i, int n, vector<int>&dp){
-        if(i == n) return 1;
-        if(i > n) return 0;
-        if(dp[i] != -1){
-            return dp[i];
-        }
-        return dp[i] = count(i+1,n,dp) + count(i+2,n,dp); 
-    }
-
     int climbStairs(int n) {
+
         vector<int>dp(n+2,-1);
-        return count(0,n,dp);
+        dp[n] = 1;
+        dp[n+1] = 0;
+        for(int i = n-1; i >= 0; i--){
+            dp[i] = dp[i+1] + dp[i+2];
+        }
+        return dp[0];
     }
 };
