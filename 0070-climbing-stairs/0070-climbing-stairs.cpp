b0afcb0de67d2794addd767103f325c04eea2f73
PRAKHARSAHUJI <148934122+PRAKHARSAHUJI@users.noreply.github.com>
2025-02-04 10:45:39 +0530

    Time: 0 ms (100%), Space: 8.5 MB (35.26%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,21 +1,28 @@
 // ------------------------------------------
-// TLE
-
-class Solution {
-public:
-    int climbStairs(int n) {
-        if(n==0) return 1;
-        if(n==1) return 1;
-        return climbStairs(n-1) + climbStairs(n-2);
-    }
-};
-
-// ------------------------------------------
+// TLE at n = 44
 
 // class Solution {
 // public:
 //     int climbStairs(int n) {
 //         if(n==0) return 1;
-//         return climStairs(n-1) + climStairs(n-2);
+//         if(n==1) return 1;
+//         return climbStairs(n-1) + climbStairs(n-2);
 //     }
 // };
+
+// ------------------------------------------
+
+class Solution {
+public:
+    int count(int n, vector<int>&dp){
+        if(n <= 1) return 1;
+        if(dp[n] != -1){
+            return dp[n];
+        }
+        return dp[n] = count(n-1,dp) + count(n-2,dp);
+    }
+    int climbStairs(int n) {
+        vector<int>dp(n+1,-1);
+        return count(n,dp);
+    }
+};
