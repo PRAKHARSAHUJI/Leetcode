8213149d4cc53a4cb5359e2a38b05b4ae060c503
PRAKHARSAHUJI <148934122+PRAKHARSAHUJI@users.noreply.github.com>
2025-02-14 20:18:48 +0530

    Time: 31 ms (10.78%), Space: 20.1 MB (11.5%) - LeetHub

---
+++ 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string sortVowels(string s) {
+        int n = s.length();
+        char* arr = new char[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = '.'; 
+        }
+        vector <char> ans; 
+        for (int i = 0; i < n; i++) {
+            if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' ||
+                s[i] == 'u' || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' ||
+                s[i] == 'O' || s[i] == 'U')
+            {
+                ans.push_back(s[i]);
+            }
+            else{
+                arr[i] = s[i];
+            }
+        }
+        sort(ans.rbegin(), ans.rend());
+
+        for(int i = 0; i < n; i++){
+            if(arr[i] == '.'){
+                char ch = ans.back();  
+                ans.pop_back(); 
+                arr[i] = ch;
+            }
+        }
+        string res = "";
+        for(int i = 0; i < n; i++){
+            res += arr[i];
+        }
+        return res;
+    }
+};
