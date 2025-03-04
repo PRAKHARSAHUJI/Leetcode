4ac018b4816840ca846eb721199cc9691385de70
PRAKHARSAHUJI <148934122+PRAKHARSAHUJI@users.noreply.github.com>
2025-03-04 20:35:28 +0530

    Time: 0 ms (100%), Space: 63.6 MB (12.99%) - LeetHub

--- 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.cpp
+++ 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.cpp
@@ -1,3 +1,6 @@
+/*
+
+// -------------------------- APPROACH 1 -------------------------- 
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& v) {
@@ -5,17 +8,52 @@ public:
         int start = 1;
         int end = n - 1;
 
-        while(start <= end){
+        while (start <= end) {
             int mid = start + (end - start) / 2;
-            
-            if((v[mid] > v[mid - 1]) && (v[mid] > v[mid + 1])){
+
+            if ((v[mid] > v[mid - 1]) && (v[mid] > v[mid + 1])) {
                 return mid;
-            }else if( (v[mid] > v[mid - 1]) && (v[mid] < v[mid + 1])){
+            } else if ((v[mid] > v[mid - 1]) && (v[mid] < v[mid + 1])) {
                 start = mid + 1;
-            }else if( (v[mid] < v[mid - 1]) && (v[mid] > v[mid + 1])){
+            } else if ((v[mid] < v[mid - 1]) && (v[mid] > v[mid + 1])) {
                 end = mid - 1;
             }
         }
+        return 0;
+    }
+};
+
+
+*/
+
+
+// -------------------------- APPROACH 2 -------------------------- 
+
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& v) {
+        int n = v.size();
+        int start = 0;
+        int end = n - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (mid > 0 && mid < v.size() - 1) { 
+                if ((v[mid] > v[mid - 1]) && (v[mid] > v[mid + 1])) {
+                    return mid;
+                } else if ((v[mid] > v[mid - 1]) && (v[mid] < v[mid + 1])) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            } else if (mid == 0) { 
+                return (v[0] > v[1]) ? 0 : 1;
+            } else { 
+                return v.size() - 1;
+            }
+        }
+
         return 0;
     }
 };
