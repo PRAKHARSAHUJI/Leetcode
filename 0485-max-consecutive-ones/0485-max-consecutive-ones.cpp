4ecf4ab50eb9b3e18d27f1ba36bc617a3b02832b
PRAKHARSAHUJI <148934122+PRAKHARSAHUJI@users.noreply.github.com>
2025-05-02 23:09:54 +0530

    Time: 0 ms (100%), Space: 50.1 MB (65.06%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
@@ -1,34 +1,42 @@
 class Solution {
 public:
-    // int findMaxConsecutiveOnes(vector<int>& arr) {
-    //     int n = arr.size();
-    //     int ans = 0;
-    //     int res = 0;
-    //     for(int i = 0; i < n; i++){
-    //         while(arr[i] == 1){
-    //             ans++;
-    //             i++;
-    //             res = max(res,ans);
-    //         }
-    //         if(arr[i] == 0){
-    //             ans = 0;
-    //         }
-    //     }
-    //     return res;
-    // }
-
     int findMaxConsecutiveOnes(vector<int>& arr) {
         int n = arr.size();
         int ans = 0;
         int res = 0;
-        for(int i = 0; i < n; i++) {
-            if(arr[i] == 1) {
+        for(int i = 0; i < n;){
+            while((i< n) && (arr[i] == 1 )){
                 ans++;
-                res = max(res, ans);
-            } else {
+                i++;
+                res = max(res,ans);
+            }
+            if((i< n) && (arr[i] == 0)){
                 ans = 0;
+                i++;
             }
         }
         return res;
+
     }
 };
+
+
+// ---------------------METHOD 2 -------------------
+
+// class Solution {
+// public:
+//     int findMaxConsecutiveOnes(vector<int>& arr) {
+//         int n = arr.size();
+//         int ans = 0;
+//         int res = 0;
+//         for(int i = 0; i < n; i++) {
+//             if(arr[i] == 1) {
+//                 ans++;
+//                 res = max(res, ans);
+//             } else {
+//                 ans = 0;
+//             }
+//         }
+//         return res;
+//     }
+// };
